85a750a514b0e55869bb44a7da4521b32b20f0a8
mdeejay <mdjrussia@gmail.com>
2019-07-20 06:19:09 +0200

    lavender: Switch to Snap
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap2
+    Snap
 
 # CNE
 PRODUCT_PACKAGES += \
